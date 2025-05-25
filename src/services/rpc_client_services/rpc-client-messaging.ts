cf7cebcb6bf1aa903c1cb23d088c5625dbfffb83
hoangpc <https://github.com/HoangPC98>
2025-05-26 00:24:31 +0700

    add logic code for chat-service 26-05

---
+++ src/services/rpc_client_services/rpc-client-messaging.ts
@@ -0,0 +1,39 @@
+import path from 'path';
+import * as GRPC from '@grpc/grpc-js';
+import * as ProtoLoader from '@grpc/proto-loader';
+import { RpcRequestServiceAbstract } from './rpc-client-abstract';
+import { IGrpcClientRequest } from 'src/interfaces/grpc-client.interface';
+import { request } from 'http';
+
+export class MessagingClientService extends RpcRequestServiceAbstract {
+
+  constructor() {
+    super();
+    // Define the path to the proto file
+    this.serviceHost = process.env.messaging_RPC_HOST || '0.0.0.0:9001';
+    this.serviceProtoPath = path.join(process.cwd(), '../ecom-protos-grpc/messaging/messaging.proto');
+
+    // Load the proto file
+    const messagingPackageDefinition = ProtoLoader.loadSync(this.serviceProtoPath);
+    const messagingProto = GRPC.loadPackageDefinition(messagingPackageDefinition).Messaging as any;
+
+    // Create the service stub
+    this.serviceClientCall = new messagingProto.messagingService(this.serviceHost, GRPC.credentials.createInsecure());
+  }
+
+  clientRequest(call: IGrpcClientRequest, callback: any) {
+    console.log('--> RPC MessagingRequest..', call)
+    this.serviceClientCall[call.method]({ ...call.message}, (err: any, response: any) => {
+      console.log('--> RPC MessagingResponse..', response, err)
+      if (err) {
+        console.log('Error', err);
+        callback(err);
+      }
+      else {
+        callback(null, response);
+      }
+    });
+  }
+
+  clientmessaging() { }
+}
